997085bdd072b807b2a34d9fad216b6de1ee3110
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2025-04-04 14:51:00 +0000

    Update and rename 1-clinvar-preprocessing.R to delete-1-clinvar-preprocessing.R

--- domain-analysis/clinvar/delete-1-clinvar-preprocessing.R
+++ domain-analysis/clinvar/delete-1-clinvar-preprocessing.R
@@ -1,1 +1,3 @@
 # This code inputs a clinvar missense variant file for a gene and outputs a tab delimited file containing chromosome, genomic position, original base, variant base.
+
+# That file is then run on the website https://www.ebi.ac.uk/thornton-srv/databases/cgi-bin/VarSite/GetPage.pl?varmap=TRUE# to get the protein position
