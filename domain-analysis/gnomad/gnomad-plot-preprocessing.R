99666d10244149041dd841d829f88917bba98c60
UlfurBen <99927907+UlfurBen@users.noreply.github.com>
2025-04-04 14:20:06 +0000

    Create gnomad-plot-preprocessing.R

---
+++ domain-analysis/gnomad/gnomad-plot-preprocessing.R
@@ -0,0 +1,3 @@
+# This code inputs gnomad variants file for a gene with protein position included and domains for that gene.
+# The code counts number of gnomad variants in each domain.
+# The code then outputs gnomad variant count for each domain
